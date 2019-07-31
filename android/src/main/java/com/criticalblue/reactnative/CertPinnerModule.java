f379010e32bd3060ab1b363788c88eeaa485712f
Jasper Modderaar <jasperjm@users.noreply.github.com>
2019-07-31 10:09:34 +0200

    Add module annotation

--- android/src/main/java/com/criticalblue/reactnative/CertPinnerModule.java
+++ android/src/main/java/com/criticalblue/reactnative/CertPinnerModule.java
@@ -4,8 +4,12 @@ package com.criticalblue.reactnative;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.modules.network.OkHttpClientProvider;
+import com.facebook.react.module.annotations.ReactModule;
 
+@ReactModule(name = CertPinnerModule.MODULE_NAME)
 public class CertPinnerModule extends ReactContextBaseJavaModule {
+  final static String MODULE_NAME = "CertPinner";
+
   private final ReactApplicationContext reactContext;
 
   public CertPinnerModule(ReactApplicationContext reactContext) {
@@ -15,6 +19,6 @@ public class CertPinnerModule extends ReactContextBaseJavaModule {
 
   @Override
   public String getName() {
-    return "CertPinner";
+    return this.MODULE_NAME;
   }
 }
